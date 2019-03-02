8563e34b210c4990a9164004e094a595106fad01
Natalya <tullochnatalya@gmail.com>
2019-03-02 19:36:51 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let request = new XMLHttpRequest();
+let url = "https://api.adviceslip.com/advice/search/days";
+request.open("GET", url, true)
+
+//Begin acessing JSON data here
+request.onload = function(){
+    let data = JSON.parse(this.response);
+    let advice = document.getElementById('advice');
+    
+    if (request.status >= 200 && request.status < 400) {
+       console.log('data', data)
+       advice.innerHTML = data.slips[0].advice;
+    }
+    
+};
+
+request.send();
